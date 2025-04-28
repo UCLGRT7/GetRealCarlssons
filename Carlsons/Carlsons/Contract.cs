c91f91cd4afa32097f3be0b136397dc1a5fc6b82
WildclawsDK <68854789+WildclawsDK@users.noreply.github.com>
2025-04-28 20:33:23 +0200

    Added base classes

---
+++ Carlsons/Carlsons/Contract.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Carlssons
+{
+    public class Contract
+    {
+        public DateTime OfferSent { get; set; }
+        public DateTime PaymentReceived { get; set; }
+        public DateTime OfferConfirmed { get; set; }
+        public double Price { get; set; }
+        public int InvoiceNumber { get; set; }
+
+        public Contract(DateTime offerSent, DateTime paymentReceived, DateTime offerConfirmed, double price, int invoiceNumber)
+        {
+            OfferSent = offerSent;
+            PaymentReceived = paymentReceived;
+            OfferConfirmed = offerConfirmed;
+            Price = price;
+            InvoiceNumber = invoiceNumber;
+        }
+
+        public void EditPaymentReceived()
+        {
+            
+        }
+
+        public void EditOfferSent()
+        {
+            
+        }
+
+        public void EditPrice()
+        {
+            
+        }
+
+        public void EditInvoiceNumber()
+        {
+            
+        }
+    }
+}
---
+++ Carlsons/Carlsons/Project.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Carlssons
+{
+    public class Project
+    {
+        public string CaseNumber { get; set; }
+        public string AddressOfDelivery { get; set; }
+        public DateTime DateOfDelivery { get; set; }
+        public double PriceEstimate { get; set; }
+        public int Scale { get; set; }
+
+        public Project(string caseNumber, string addressOfDelivery, DateTime dateOfDelivery, double priceEstimate, int scale)
+        {
+            CaseNumber = caseNumber;
+            AddressOfDelivery = addressOfDelivery;
+            DateOfDelivery = dateOfDelivery;
+            PriceEstimate = priceEstimate;
+            Scale = scale;
+        }
+
+        public void CreateProject()
+        {
+            
+        }
+
+        public void SearchCaseNumber()
+        {
+            
+        }
+
+        public void SearchAddress()
+        {
+            
+        }
+
+        public void SearchScale()
+        {
+            
+        }
+
+        public void EditDate()
+        {
+            
+        }
+    }
+}
