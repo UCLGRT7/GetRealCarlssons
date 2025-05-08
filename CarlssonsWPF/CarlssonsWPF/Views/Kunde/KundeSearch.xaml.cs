fabb9ec65d784b9e57d4262e4978aac4471d7bce
steff <steffen.hansson@outlook.com>
2025-05-08 14:54:55 +0200

    Ver. 1.0 af søgesideopdatering

--- CarlssonsWPF/CarlssonsWPF/Views/Kunde/KundeSearch.xaml.cs
+++ CarlssonsWPF/CarlssonsWPF/Views/Kunde/KundeSearch.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,11 +14,16 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using CarlssonsWPF.Model;
 using CarlssonsWPF.Views.Projekt;
 
 namespace CarlssonsWPF.Views.Kunde
 {
-    public partial class KundeSearch : Page
+    class CustomerSpecViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+        public ObservableCollection<Customer> customers { get; set; } = new ObservableCollection<Customer>();
+        public partial class KundeSearch : Page
     {
         private Frame _frame;
         public KundeSearch(Frame frame)
@@ -28,6 +35,9 @@ namespace CarlssonsWPF.Views.Kunde
         private void KundeSearch_Click(object sender, RoutedEventArgs e)
         {
             _frame.Navigate(new SearchResults(_frame));
+            string searchTerm = SearchBox.Text;
+            FetchData(searchTerm);     
+
         }
         private void KundeMainWindow_Click(object sender, RoutedEventArgs e)
         {
