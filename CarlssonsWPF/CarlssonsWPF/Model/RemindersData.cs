434bcf109e2d5f13c035c6ba720e27b3aac3a34e
WildclawsDK <68854789+WildclawsDK@users.noreply.github.com>
2025-05-13 13:04:44 +0200

    merge

---
+++ CarlssonsWPF/CarlssonsWPF/Model/RemindersData.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarlssonsWPF.Model
+{
+    public class RemindersData
+    {
+        public string CaseNumber { get; set; }
+        public string CustomerName { get; set; }
+        public DateTime? OfferSentDate { get; set; }
+        public DateTime? LastModified { get; set; }
+        public DateTime? DateOfDelivery { get; set; }
+        public DateTime? PaymentReceivedDate { get; set; }
+        public string Status { get; set; }
+        public string OfferConfirmed { get; set; }
+        public string IsPaymentRecieved { get; set; }
+
+
+
+
+    }
+}
