6056e1c0a4b09e2007edd5248ba042590ae1b0b7
WildclawsDK <68854789+WildclawsDK@users.noreply.github.com>
2025-05-16 15:04:55 +0200

    Merge

--- CarlssonsWPF/CarlssonsWPF/Data/FileRepositories/FileServiceRepository.cs
+++ CarlssonsWPF/CarlssonsWPF/Data/FileRepositories/FileServiceRepository.cs
@@ -73,9 +73,9 @@ namespace CarlssonsWPF.Data.FileRepositories
             return services;
         }
 
-        public IEnumerable<Services> GetByServiceType(string serviceType)
+        public IEnumerable<Services> GetByServiceEntry(string name)
         {
-            return GetAll().Where(s => s.Name.Contains(serviceEntry, StringComparison.OrdinalIgnoreCase));
+            return GetAll().Where(s => s.Name.Contains(name, StringComparison.OrdinalIgnoreCase));
         }
 
         public IEnumerable<Services> GetByComplexity(int complexity)
