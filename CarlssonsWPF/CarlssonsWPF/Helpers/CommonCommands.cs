e90599da23cd080510e56e11f5681391dc52f46a
Outlashed <Outlashed@Gmail.com>
2025-05-20 00:23:26 +0200

    Merge

---
+++ CarlssonsWPF/CarlssonsWPF/Helpers/CommonCommands.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Navigation;
+using CarlssonsWPF.ViewModel;
+using CarlssonsWPF.Model;
+
+namespace CarlssonsWPF.Helpers
+{
+    public static class CommonCommands
+    {
+        public static ICommand GoBack { get; } = new RelayCommand(ExecuteGoBack);
+        public static ICommand Create { get; } = new RelayCommand(ExecuteCreate);
+
+        private static void ExecuteGoBack(object parameter)
+        {
+            if (parameter is FrameworkElement fe)
+            {
+                var navService = NavigationService.GetNavigationService(fe);
+
+                if (navService != null && navService.CanGoBack)
+                {
+                    navService.GoBack();
+                }
+            }
+        }
+
+        private static void ExecuteCreate(object parameter)
+        {
+            var frame = Application.Current.MainWindow?.FindName("MainFrame") as Frame;
+
+            switch (parameter)
+            {
+                case CreateProjectViewModel projectVm:
+                    {
+                        projectVm.CreateProject();
+                        var latestProject = projectVm.projects.LastOrDefault();
+                        if (latestProject == null)
+                        {
+                            MessageBox.Show("Projekt blev ikke oprettet korrekt.");
+                            return;
+                        }
+
+                        if (frame != null)
+                        {
+                            var view = new Views.Projekt.ViewProjectView(frame, latestProject);
+                            frame.Navigate(view);
+                        }
+
+                        break;
+                    }
+
+                case AddCustomerViewModel customerVm:
+                    {
+                        customerVm.AddCustomer();
+                        var latestCustomer = customerVm.customers.LastOrDefault();
+                        if (latestCustomer == null)
+                        {
+                            MessageBox.Show("Kunde blev ikke oprettet korrekt.");
+                            return;
+                        }
+
+                        if (frame != null)
+                        {
+                            var view = new Views.Kunde.CustomerSpecPage(frame, latestCustomer);
+                            frame.Navigate(view);
+                        }
+
+                        break;
+                    }
+
+                default:
+                    MessageBox.Show("Ugyldig ViewModel – kun CreateProjectViewModel eller AddCustomerViewModel understøttes.");
+                    break;
+            }
+        }
+
+    }
+}
