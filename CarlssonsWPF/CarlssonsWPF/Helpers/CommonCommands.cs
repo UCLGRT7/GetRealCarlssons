03ef59b5ff8613852c97abb575c2aec08990a87e
Outlashed <Outlashed@Gmail.com>
2025-05-20 10:27:16 +0200

    Name fix

--- CarlssonsWPF/CarlssonsWPF/Helpers/CommonCommands.cs
+++ CarlssonsWPF/CarlssonsWPF/Helpers/CommonCommands.cs
@@ -1,12 +1,41 @@
 ﻿using System;
 using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Input;
 using System.Windows.Navigation;
 using CarlssonsWPF.ViewModel;
 
+namespace CarlssonsWPF.Helpers;
+
 public static class CommonCommands
 {
     public static ICommand CancelAndGoBackCommand { get; } = new RelayCommand(ExecuteCancel);
+
+    public static ICommand GoBackAndReload { get; } = new RelayCommand(ExecuteGoBackAndReload);
+
+    private static void ExecuteGoBackAndReload(object parameter)
+    {
+        if (parameter is Frame frame)
+        {
+            if (frame.CanGoBack)
+                frame.RemoveBackEntry(); // Fjern nuværende side hvis ønsket
+
+            var currentPage = frame.Content as Page;
+            var pageType = currentPage?.GetType();
+            if (pageType != null)
+            {
+                var newPage = (Page)Activator.CreateInstance(pageType);
+
+                if (newPage.DataContext is IReloadableViewModel vm)
+                {
+                    vm.LoadData(); // Kald på tværs af alle ViewModels
+                }
+
+                frame.Navigate(newPage);
+            }
+        }
+    }
+
 
     private static void ExecuteCancel(object parameter)
     {
